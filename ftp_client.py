310de476cdf6daaadd6a8ebbe6d93c244768c342
Amiay Narayan <amiaynarayan@macftp.local>
2022-08-02 10:16:28 +0530

    Test the client by changing the file to download from server

--- ftp_client.py
+++ ftp_client.py
@@ -1,9 +1,9 @@
 import ftplib
 import os
 
-host = '192.168.1.23'
+host = '192.168.1.23'   # a temporary server that I crearted on my lenovo laptop
 password = 'pass'
-port = 2121
+port = 2121             # this should be more than 1023
 username = 'user'
 
 ftp_client = ftplib.FTP()
@@ -14,14 +14,11 @@ ftp_client.login(username, password)
 max_tries = 3
 #local_path = '/Users/amiaynarayan/Work/s3/analyses/692/39690/ERR194151_2.10K.fastq.gz'
 #remote_path = 'ERR194151_2.10K.fastq.gz'
-local_path = '/Users/amiaynarayan/Work/s3/analyses/692/39690/local_mac'
 remote_path = 'ftp_server.py'
+local_path = f'/Users/amiaynarayan/learn/ftp/{remote_path}'    # if remote path is just a name
 dir_path = os.path.dirname(remote_path)
 file_name = os.path.basename(remote_path)
 ftp_client.cwd(f'/{dir_path}')
-print('Changing directory ', f'/{dir_path}')
-print('on the local the file should be named as : ', local_path)
-print('directory was changed, to ', dir_path,'and file we will search there is', f'{file_name}')
 with open(local_path, 'wb') as hand:
   ftp_client.retrbinary(f'RETR {file_name}', hand.write)
 ftp_client.quit()
